57bae4faf4a6d5956ebe9b2a16657d8551060476
Md Tauheed <tauheedmd747@gmail.com>
2026-06-03 02:47:48 +0530

    Add Metro bundler entry point

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js
+ * Metro bundler entry point for React Native
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
