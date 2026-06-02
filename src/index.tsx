b5c15526ce7d89ca01dfe9c62ba5c3b6e9d4027b
Md Tauheed <tauheedmd747@gmail.com>
2026-06-03 02:50:12 +0530

    Add React Native entry point

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * index.tsx
+ * React Native entry point
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from '../app.json';
+
+AppRegistry.registerComponent(appName, () => App);
